b94f0ad8627c8eb6fcb5798971bbc4b258691e9d
Wysted <vladimirbesser@gmail.com>
2023-02-06 18:18:22 -0300

    Add effect lazy loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,8 @@ import { AiOutlineCopy } from 'react-icons/ai';
 import { toast, Toaster } from 'react-hot-toast';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function Card({ name, title, x, y, z, descrip, created, img , id}){
     const [viewNotification,setViewNotification] = useState(false)
@@ -11,19 +13,21 @@ function Card({ name, title, x, y, z, descrip, created, img , id}){
             setViewNotification(true)
         })
     }
-
-    
     
     return (
         <div className="relative h-full w-full mx-auto m-4 shadow-lg">
-            {  viewNotification ? <Toaster/> : ""}
+            {viewNotification ? <Toaster /> : ""}
             <div className=" shadow-lg  rounded-lg bg-slate-50 h-full flex flex-col justify-evenly p-4">
                 <div className="flex relative rounded-lg overflow-hidden h-52 ">
-                    <Link to={`/${id}`} className="absolute inset-0 grid justify-items-stretch cursor-pointer">
-                        <img
+                    <Link
+                        to={`/${id}`}
+                        className="absolute inset-0 grid justify-items-stretch cursor-pointer"
+                    >
+                        <LazyLoadImage
                             src={img}
                             alt="Imagen minecraft"
                             className="object-fill"
+                            effect='blur'
                         />
                     </Link>
                 </div>
@@ -36,15 +40,16 @@ function Card({ name, title, x, y, z, descrip, created, img , id}){
                         <span className="mx-2">x : {x}</span>
                         <span className="mx-2">y : {y}</span>
                         <span className="mx-2">z : {z}</span>
-                        <AiOutlineCopy className='h-8 w-8 cursor-copy text-sky-300' onClick={handleClickCopy}/>
+                        <AiOutlineCopy
+                            className="h-8 w-8 cursor-copy text-sky-300"
+                            onClick={handleClickCopy}
+                        />
                     </p>
                 </div>
                 {/* Descripcion */}
                 <div className="mt-8 h-max">
                     <p className="inline-flex flex-col xl:flex-row xl:items-center text-gray-800 p-2">
-                        <span className="mt-2 xl:mt-0">
-                            {descrip}
-                        </span>
+                        <span className="mt-2 xl:mt-0">{descrip}</span>
                     </p>
                 </div>
                 {/* Nombre */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import SearchBar from "./SearchBar.jsx";
 import { Link } from "react-router-dom";
-import Logo_header from '../assets/images/logo_header.png'
+import Logo_header from '../assets/images/logo_header.svg'
 
 function Header(){
 
 
     return (
         <>
-        <header  >
-            <nav className="h-full w-full flex flex-wrap items-center justify-around bg-gray-100 text-gray-500  focus:text-gray-700 shadow-lg">
+        <header >
+            <nav  className="h-full w-full flex flex-wrap items-center justify-around bg-gray-100 text-gray-500  focus:text-gray-700 shadow-lg">
                 
-                    <img src={Logo_header} alt="Logo pagina" className="w-72 "/>
+                    <img src={Logo_header} alt="Logo pagina" className="w-96 "/>
 
                     <Link to='/' className="flex  h-20 text-xl hover:bg-slate-300 items-center px-2 ">
                         Home
